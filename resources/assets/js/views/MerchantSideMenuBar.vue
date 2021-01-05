2a41f90006375e2d5db54927e0a9c5a7edf7788b
MD. Nurul Momen <mdnurulmomen.bd@gmail.com>
2021-01-05 16:09:08 +0600

    Merchant dashboard

---
+++ resources/assets/js/views/MerchantSideMenuBar.vue
@@ -0,0 +1,99 @@
+<template>
+
+	<div class="pcoded-main-container">
+		<div class="pcoded-wrapper">
+
+			<nav class="pcoded-navbar">
+				<div class="nav-list">
+						
+					<div class="pcoded-inner-navbar main-menu">
+						<div class="pcoded-navigation-label">Navigation</div>
+
+						<ul class="pcoded-item pcoded-left-item">
+
+							<li 
+								class="pcoded-hasmenu" 
+								:class="['home', 'dashboar-2', 'dashboar-3'].includes(currentRouteName) ? 'active pcoded-trigger' : ''"
+							>
+								<a href="javascript:void(0)" class="waves-effect waves-dark">
+									<span class="pcoded-micon">
+										<i class="feather icon-home"></i>
+									</span>
+									<span class="pcoded-mtext">Home</span>
+									<!-- <span class="pcoded-badge label label-warning">NEW</span> -->
+								</a>
+								<ul class="pcoded-submenu">
+									<li :class="currentRouteName=='home' ? 'active' : ''">
+										<router-link :to="{ name: 'home' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">Dashboard</span>
+										</router-link>
+									</li>
+									<li :class="currentRouteName=='dashboar-2' ? 'active' : ''">
+										<router-link :to="{ name: 'dashboar-2' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">Dashboard 2</span>
+										</router-link>
+									</li>
+								</ul>
+							</li>
+
+							<li 
+								class="pcoded-hasmenu" 
+								:class="['products', 'product-dispatches'].includes(currentRouteName) ? 'active pcoded-trigger' : ''"
+							>
+								<a href="javascript:void(0)" class="waves-effect waves-dark">
+									<span class="pcoded-micon">
+										<i class="feather icon-box"></i>
+									</span>
+									<span class="pcoded-mtext">Products</span>
+									<!-- <span class="pcoded-badge label label-warning">NEW</span> -->
+								</a>
+								<ul class="pcoded-submenu">
+									<li :class="currentRouteName=='products' ? 'active' : ''">
+										<router-link :to="{ name: 'products' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">My Products</span>
+										</router-link>
+									</li>
+									<li :class="currentRouteName=='product-dispatches' ? 'active' : ''">
+										<router-link :to="{ name: 'product-dispatches' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">Dispatches</span>
+										</router-link>
+									</li>
+								</ul>
+							</li>
+
+							<li :class="currentRouteName=='requests' ? 'active' : ''">
+								<router-link :to="{ name: 'requests' }" class="waves-effect waves-dark">
+									<span class="pcoded-micon">
+										<i class="feather icon-aperture"></i>
+									</span>
+									<span class="pcoded-mtext">Requests</span>
+								</router-link>
+							</li>
+
+						</ul>
+					</div>
+
+				</div>
+			</nav>
+
+			<router-view></router-view>
+
+			<div id="styleSelector">
+			</div>
+
+		</div>
+	</div>
+
+</template>
+
+<script>
+    export default {
+
+    	computed: {
+		    currentRouteName() {
+		        return this.$route.name;
+		    }
+		}
+
+    }
+</script>
