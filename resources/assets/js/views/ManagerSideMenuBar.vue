6db067d089265bf7533465f255a8e7b7f7a53dfe
Md. Nurul Momen <mdnurulmomen.bd@gmail.com>
2021-04-01 22:10:15 +0600

    Applying roles and permission on Manager dashboard

--- resources/assets/js/views/ManagerSideMenuBar.vue
+++ resources/assets/js/views/ManagerSideMenuBar.vue
@@ -10,10 +10,9 @@
 						<div class="pcoded-navigation-label">Navigation</div>
 
 						<ul class="pcoded-item pcoded-left-item">
-
 							<li 
 								class="pcoded-hasmenu" 
-								:class="['home', 'dashboar-2'].includes(currentRouteName) ? 'active pcoded-trigger' : ''"
+								:class="['home','dashboar-2','dashboar-3'].includes(currentRouteName) ? 'active pcoded-trigger' : ''"
 							>
 								<a href="javascript:void(0)" class="waves-effect waves-dark">
 									<span class="pcoded-micon">
@@ -33,18 +32,209 @@
 											<span class="pcoded-mtext">Dashboard 2</span>
 										</router-link>
 									</li>
+									<li :class="currentRouteName=='dashboar-3' ? 'active' : ''">
+										<router-link :to="{ name: 'dashboar-3' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">Dashboard 3</span>
+										</router-link>
+									</li>
 								</ul>
 							</li>
 
-							<li :class="currentRouteName=='my-warehouses' ? 'active' : ''">
-								<router-link :to="{ name: 'my-warehouses' }" class="waves-effect waves-dark">
+							<!-- special routes -->
+							<li 
+								class="pcoded-hasmenu" 
+								:class="['storage-types', 'containers', 'rent-periods', 'variation-types', 'variations'].includes(currentRouteName) ? 'active pcoded-trigger' : ''" 
+								v-if="userHasPermissionTo('view-asset-index')"
+							>
+								<a href="javascript:void(0)" class="waves-effect waves-dark">
 									<span class="pcoded-micon">
-										<i class="feather icon-aperture"></i>
+										<i class="feather icon-box"></i>
 									</span>
-									<span class="pcoded-mtext">Warehouses</span>
+									<span class="pcoded-mtext">Assets</span>
+									<!-- <span class="pcoded-badge label label-danger">NEW</span> -->
+								</a>
+								<ul class="pcoded-submenu">
+									<li :class="currentRouteName=='storage-types' ? 'active' : ''">
+										<router-link :to="{ name: 'storage-types' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">Storage Types</span>
+										</router-link>
+									</li>
+
+									<li :class="currentRouteName=='containers' ? 'active' : ''">
+										<router-link :to="{ name: 'containers' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">Container Types</span>
+										</router-link>
+									</li>
+
+									<li :class="currentRouteName=='rent-periods' ? 'active' : ''">
+										<router-link :to="{ name: 'rent-periods' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">Rent Periods</span>
+										</router-link>
+									</li>
+
+									<li 
+										class="pcoded-hasmenu" 
+										:class="['variation-types', 'variations'].includes(currentRouteName) ? 'active pcoded-trigger' : ''"
+									>
+										<a href="javascript:void(0)" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">Variations</span>
+										</a>
+										<ul class="pcoded-submenu">
+											<li :class="currentRouteName=='variation-types' ? 'active' : ''">
+												<router-link :to="{ name: 'variation-types' }" class="waves-effect waves-dark">
+													<span class="pcoded-mtext">Types</span>
+												</router-link>
+											</li>
+											<li :class="currentRouteName=='variations' ? 'active' : ''">
+												<router-link :to="{ name: 'variations' }" class="waves-effect waves-dark">
+													<span class="pcoded-mtext">Variations</span>
+												</router-link>
+											</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+
+							<li 
+								:class="currentRouteName=='roles' ? 'active' : ''" 
+								v-if="userHasPermissionTo('view-role-index')"
+							>
+								<router-link :to="{ name: 'roles' }" class="waves-effect waves-dark">
+									<span class="pcoded-micon">
+										<i class="fa fa-tasks" aria-hidden="true"></i>
+									</span>
+									<span class="pcoded-mtext">
+										Roles
+									</span>
 								</router-link>
 							</li>
 
+							<li 
+								class="pcoded-hasmenu" 
+								:class="['owners','my-warehouses','warehouses'].includes(currentRouteName) ? 'active pcoded-trigger' : ''" 
+							>
+								<a href="javascript:void(0)" class="waves-effect waves-dark">
+									<span class="pcoded-micon">
+										<i class="feather icon-command"></i>
+									</span>
+									<span class="pcoded-mtext">Warehouse</span>
+									<!-- <span class="pcoded-badge label label-warning">NEW</span> -->
+								</a>
+								<ul class="pcoded-submenu">
+									<li :class="currentRouteName=='owners' ? 'active' : ''" v-if="userHasPermissionTo('view-warehouse-owner-index')">
+										<router-link :to="{ name: 'owners' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">Owners</span>
+										</router-link>
+									</li>
+
+									<li :class="currentRouteName=='my-warehouses' ? 'active' : ''">
+										<router-link :to="{ name: 'my-warehouses' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">My Warehouses</span>
+										</router-link>
+									</li>
+
+									<li :class="currentRouteName=='warehouses' ? 'active' : ''" v-if="userHasPermissionTo('view-warehouse-index')">
+										<router-link :to="{ name: 'warehouses' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">All Warehouses</span>
+										</router-link>
+									</li>
+								</ul>
+							</li>
+
+							<li :class="currentRouteName=='managers' ? 'active' : ''" 
+								v-if="userHasPermissionTo('view-manager-index')"
+							>
+								<router-link :to="{ name: 'managers' }" class="waves-effect waves-dark">
+									<span class="pcoded-micon">
+										<i class="fa fa-user"></i>
+									</span>
+									<span class="pcoded-mtext">
+										Managers
+									</span>
+								</router-link>
+							</li>
+
+							<li :class="currentRouteName=='merchants' ? 'active' : ''" 
+								v-if="userHasPermissionTo('view-merchant-index')"
+							>
+								<router-link :to="{ name: 'merchants' }" class="waves-effect waves-dark">
+									<span class="pcoded-micon">
+										<i class="fa fa-users"></i>
+									</span>
+									<span class="pcoded-mtext">
+										Merchants
+									</span>
+								</router-link>
+							</li>
+
+							<li 
+								class="pcoded-hasmenu" 
+								:class="['my-products','product-categories','products','product-stocks'].includes(currentRouteName) ? 'active pcoded-trigger' : ''"
+							>
+								<a href="javascript:void(0)" class="waves-effect waves-dark">
+									<span class="pcoded-micon">
+										<i class="fab fa-product-hunt"></i>
+									</span>
+									<span class="pcoded-mtext">Product</span>
+									<!-- <span class="pcoded-badge label label-warning">NEW</span> -->
+								</a>
+								<ul class="pcoded-submenu">
+									<!-- 
+									<li :class="currentRouteName=='my-products' ? 'active' : ''">
+										<router-link :to="{ name: 'my-products' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">My Products</span>
+										</router-link>
+									</li>
+									 -->
+									<li 
+										:class="currentRouteName=='product-categories' ? 'active' : ''" 
+										v-if="userHasPermissionTo('view-product-category-index')"
+									>
+										<router-link :to="{ name: 'product-categories' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">Categories</span>
+										</router-link>
+									</li>
+									<li 
+										:class="currentRouteName=='products' ? 'active' : ''" 
+										v-if="userHasPermissionTo('view-product-index')"
+									>
+										<router-link :to="{ name: 'products' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">All Products</span>
+										</router-link>
+									</li>
+								</ul>
+							</li>
+							
+							<li 
+								class="pcoded-hasmenu" 
+								:class="['my-requisitions','requisitions'].includes(currentRouteName) ? 'active pcoded-trigger' : ''"
+							>
+								<a href="javascript:void(0)" class="waves-effect waves-dark">
+									<span class="pcoded-micon">
+										<i class="fa fa-truck"></i>
+									</span>
+									<span class="pcoded-mtext">Dispatch</span>
+									<!-- <span class="pcoded-badge label label-warning">NEW</span> -->
+								</a>
+								<ul class="pcoded-submenu">
+									<!-- 
+									<li :class="currentRouteName=='my-requisitions' ? 'active' : ''">
+										<router-link :to="{ name: 'my-requisitions' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">My Dispatches</span>
+										</router-link>
+									</li>
+									-->
+
+									<li 
+										:class="currentRouteName=='requisitions' ? 'active' : ''" 
+										v-if="userHasPermissionTo('view-requisition-index')"
+									>
+										<router-link :to="{ name: 'requisitions' }" class="waves-effect waves-dark">
+											<span class="pcoded-mtext">All Requisitions</span>
+										</router-link>
+									</li>
+								</ul>
+							</li>
 						</ul>
 					</div>
 
